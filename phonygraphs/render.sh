3ae42247b06a8822056f9099cf6c0cc705c1d246
Sudharshan <sudhars1@andrew.cmu.edu>
2021-03-25 13:37:32 -0400

    subdirectory name change

--- phonygraphs/render.sh
+++ phonygraphs/render.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 jt -t grade3 -fs 95 -altp -tfs 11 -tf ebserif -nf ebserif -nfs 115 -cellw 70% -T # https://github.com/dunovank/jupyter-themes
-jupyter nbconvert phonygraphs.ipynb --to html --no-input --no-prompt
+jupyter nbconvert phonygraphs.ipynb --to html  --output "index" --no-input --no-prompt
